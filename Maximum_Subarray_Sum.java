c41d676aee96a17e2be8699c4548fbfe87e0348a
TanujaBuri <buri.tanuja@gmail.com>
2026-07-12 21:23:44 +0530

    Add Maximum Subarray Sum solution in Java
    Implement Kadane's algorithm to find the maximum subarray sum.

---
+++ Maximum_Subarray_Sum.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        for(int i = 0;i< n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        
+        int sum = 0;
+        int maxsum = Integer.MIN_VALUE;
+        
+        for(int i = 0;i < n;i++)
+        {
+            sum += arr[i];
+            if(sum < 0)
+            {
+                sum = 0;
+            }
+            if(sum > maxsum)
+            {
+                maxsum = sum;
+            }
+        }
+        
+        System.out.println(maxsum);
+    }
+}
